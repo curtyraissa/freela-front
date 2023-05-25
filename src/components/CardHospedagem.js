b74812787e81f2ba4e15f1e64ac25d8fe5e4d3b8
curtyraissa <raissacurty@gmail.com>
2023-05-25 00:03:21 -0300

    criado component cardhospedagem

---
+++ src/components/CardHospedagem.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export default function CardHospedagem() {
+  return (
+    <>
+      <CardHospedagemContainer>
+        
+        <Bloco>
+          <p>Nome do Hotel</p>
+          <Text>PREÇO</Text>
+        </Bloco>
+      </CardHospedagemContainer>
+    </>
+  );
+}
+const Bloco = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+  color: black;
+`;
+
+const Text = styled.p`
+  font-family: "Saira Stencil One", cursive;
+  color: #ffb000;
+  font-weight: 400;
+  font-size: 14px;
+`;
+
+const CardHospedagemContainer = styled.div`
+  font-family: "Saira Stencil One", cursive;
+  font-weight: 400;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  background-color: white;
+  border-radius: 5px;
+  padding: 20px;
+`;
