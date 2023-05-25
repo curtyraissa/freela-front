40fd4eb6ef9dfc23e0df7c10e31823b2915d5a9e
curtyraissa <raissacurty@gmail.com>
2023-05-25 00:04:03 -0300

    criado HospedagemPage

--- src/pages/HospedagemPage.js
+++ src/pages/HospedagemPage.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 import Logo from "../components/Logo";
+import CardHospedagem from "../components/CardHospedagem";
+import Filtro from "../components/Filtro";
 
 export const HospedagemPage = () => {
   return (
     <>
       <Logo />
+      <Filtro placeholder="Filtrar hospedagem"/>
       <HospedagemContainer>
-        <form>
-          <input required />
-          <button type="submit">Selecionar</button>
-        </form>
+        <Text>Hospedagens em CIDADE:</Text>
+        <CardHospedagem />
       </HospedagemContainer>
     </>
   );
@@ -17,4 +18,17 @@ export const HospedagemPage = () => {
 
 const HospedagemContainer = styled.div`
   display: "flex";
+  width: 100%;
+  color: #fff;
+  margin-bottom: 10px;
+`;
+
+
+
+const Text = styled.p`
+  font-family: "Saira Stencil One", cursive;
+  font-weight: 400;
+  font-size: 20px;
+  color: white;
+  margin-bottom: 10px;
 `;
