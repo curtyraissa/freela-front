87ec4ab731145ee483d4855b414fb96388ff83c5
curtyraissa <raissacurty@gmail.com>
2023-05-25 00:03:32 -0300

    criado componente filtro

--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
-export default function Filtro() {
+export default function Filtro({placeholder}) {
   return (
     <>
-      <Search type="search" name="filtrar" placeholder="Filtrar Passagens" />
+      <Search type="search" name="filtrar" placeholder={placeholder} />
     </>
   );
 }
 
 const Search = styled.input`
   width: 100%;
-  margin-bottom: 20px;
+  margin-bottom: 50px;
   display: flex;
   align-items: center;
   justify-content: center;
