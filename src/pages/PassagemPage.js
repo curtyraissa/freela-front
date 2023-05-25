58be161fe7d1a146f81b7ae495fd68214dcb067c
curtyraissa <raissacurty@gmail.com>
2023-05-24 23:45:56 -0300

    criado tela passagem

--- src/pages/PassagemPage.js
+++ src/pages/PassagemPage.js
@@ -1,9 +1,32 @@
 import styled from "styled-components";
+import Logo from "../components/Logo";
+import Card from "../components/Card";
+import Filtro from "../components/Filtro";
 
 export const PassagemPage = () => {
-  return <PassagemContainer>PassagemPage</PassagemContainer>;
+  return (
+    <>
+      <Logo />
+      <Filtro />
+      <PassagemContainer>
+        <Text>Passagens para CIDADE:</Text>
+        <Card />
+      </PassagemContainer>
+    </>
+  );
 };
 
 const PassagemContainer = styled.div`
   display: "flex";
+  width: 100%;
+  color: #fff;
+  margin-bottom: 10px;
+`;
+
+const Text = styled.p`
+  font-family: "Saira Stencil One", cursive;
+  font-weight: 400;
+  font-size: 20px;
+  color: white;
+  margin-bottom: 10px;
 `;
